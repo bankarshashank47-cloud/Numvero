27234c98bdfa119fd06f88b8c910073a02924285
bankarshashank47-cloud <bankarshashank47@gmail.com>
2026-08-24 01:59:44 +0530

    fix: make Express SPA fallback compatible with current Express

--- server.js
+++ server.js
@@ -9,5 +9,5 @@ const dist = path.join(__dirname, 'dist');
 
 app.disable('x-powered-by');
 app.use(express.static(dist, { maxAge: '7d', index: false }));
-app.get('*', (_req, res) => res.sendFile(path.join(dist, 'index.html')));
+app.get(/.*/, (_req, res) => res.sendFile(path.join(dist, 'index.html')));
 app.listen(port, '0.0.0.0', () => console.log(`Numvero running on port ${port}`));
